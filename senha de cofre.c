69024a5cd77ad1b6cd5f161fa587039528b97ff6
Gabriel Hervatin <gvghervatin@gmail.com>
2022-09-14 19:01:12 -0300

    changes on the code

--- senha de cofre.c
+++ senha de cofre.c
@@ -2,25 +2,43 @@
 #include <stdlib.h>
 #include <locale.h>
 #define MAX 20
+#define TAM 5
 
-int barra[MAX] = {5,9,1,7,5,5,9,0,7,1}, senha[5] = {0, 1, 1, 1, 1};
+int barra[MAX] = {5,9,1,7,5,5,9,0,7,1}, senha[TAM] = {3, 1, 3, 2, 3}, senha_obt[TAM] = {0,0,0,0,0};
 int zero = 0, one = 0, five = 0, seven = 0, nine = 0;
 
 void codificador(int i)
 {
-    (barra[i] == 0) ? zero++ :
-    (barra[i] == 1) ? one++ :
-    (barra[i] == 5) ? five++ :
-    (barra[i] == 7) ? seven++ :
-    (barra[i] == 9) ? nine++ :
-    ' ';
+     switch(barra[i])
+  {
+      case 0:
+          zero++;
+          senha_obt[0] = zero;
+          break;
+      case 1:
+          one++;
+          senha_obt[1] = one;
+          break;
+      case 5:
+          five++;
+          senha_obt[2] = five;
+          break;
+      case 7:
+          seven++;
+          senha_obt[3] = seven;
+          break;
+      case 9:
+          nine++;
+          senha_obt[4] = nine;
+          break;
+  }
 }
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    int i = 0, ate, t = 0, r = 0, cont = 0;
+    int i = 0, ate, t = 0, r = 0, aux, j, flag = 0, k = 0;
 
     printf("\n\t\t\t\t\t\t-->REGRAS<--");
     printf("\n\t\t\t- Apenas três tentativas são permitidas para tentar abrir o cofre.");
@@ -29,45 +47,56 @@ int main()
 
     while(r < 3)
     {
-        printf("\t\t\t\t\t| Tentativa: %d\n", r+1);
+        printf("\t\t\t\t\t| Tentativa %d\n", r+1);
         for(t = 0; t < 4; t++)
         {
-            printf("\t\t\t\t\t| Rolamentos: %d\n", t+1);
-            printf("\t\t\t\t\t| Você escolheu a posição: ");
+            printf("\t\t\t\t\t| Deslizamento %d\n\t\t\t\t\t| Você escolheu a posição: ", t+1);
             scanf("%d",&ate);
-
+            j = 0;
+            aux = 2;
             if(ate < i)
             {
-                for(i = i; i >= ate; i--)
+                j = 1;
+                for(i = i - aux; i >= ate; i--)
                 {
                     codificador(i);
+                    printf("%d",i);
                 }
-            i++;
             }
             else
             {
                 for(i = i; i <= ate; i++)
                 {
                     codificador(i);
+                    printf("%d",i);
                 }
             }
-
-            printf("\t\t\t\t\t| Sua combinação: %d %d %d %d %d\n", zero, one, five, seven, nine);
+            printf("\t\t\t\t\t| Sua combinação: %d %d %d %d %d\n", senha_obt[0], senha_obt[1], senha_obt[2], senha_obt[3], senha_obt[4]);
             printf("\t\t\t\t\t| sua posição atual: %d\n", ate);
             printf("\t\t\t\t\t----------------------------------\n");
-
-            if(senha[0] == zero && senha[1] == one && senha[2] == five && senha[3] == seven && senha[4] == nine)
+            flag = 0;
+            for(k = 0; k < 5; k++)
+            {
+                if(senha_obt[k] == senha[k])
+                {
+                  flag++;
+                }
+            }
+            if(flag > 4)
             {
                 printf("\n\t\t\t\t\t----------------------------------\n");
-                printf("\t\t\t\t\t| Senha obtida: %d %d %d %d %d\n\t\t\t\t\t|", zero, one, five, seven, nine);
-                printf("\n\t\t\t\t\t|   -->Você abriu o cofre!!<--");
-                printf("\n\t\t\t\t\t----------------------------------\n");
-                cont = 1;
+                printf("\t\t\t\t\t| Senha obtida: %d %d %d %d %d\n", senha_obt[0], senha_obt[1], senha_obt[2], senha_obt[3], senha_obt[4]);
+                printf("\n\t\t\t\t\t    -->Você abriu o cofre!!<--\n");
                 break;
+            }
+            if(j == 1)
+            {
+                aux = 0;
+                i+=2;
             }
 
         }
-        if(cont > 0)
+        if(flag > 4)
         {
             break;
         }
@@ -90,11 +119,11 @@ int main()
         }
         else
         {
-           zero = 0;
-           one = 0;
-           five = 0;
-           seven = 0;
-           nine = 0;
+           senha_obt[0] = zero = 0;
+           senha_obt[1] = one = 0;
+           senha_obt[2] = five = 0;
+           senha_obt[3] = seven = 0;
+           senha_obt[4] = nine = 0;
            i = 0;
            r++;
         }
